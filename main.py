c6a8bdef064009d6163360a9a01115207dc5f72f
Hualet Wang <hualet@hualet.org>
2024-04-30 15:03:12 +0800

    move 360 wallpaper api to main.py

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 
 import os
+import json
 import requests
-from wallpaper_360 import get_random_wallpaper
+from random import randint
+from pprint import pprint
+
+def get_random_wallpaper_360():
+    try:
+        data = requests.get("http://cdn.apc.360.cn/index.php?c=WallPaper&a=getAllCategoriesV2&from=360chrome").json()
+        category_list = data["data"]
+        random_category = category_list[randint(0, len(category_list)-1)]
+
+        request_url = "http://wallpaper.apc.360.cn/index.php?" \
+                    "c=WallPaper&a=getAppsByCategory" \
+                    "&cid={cid}" \
+                    "&start=0" \
+                    "&from=360chrome".format(cid=random_category["id"])
+        category_wallpaper_list= requests.get(request_url).json()["data"]
+        random_wallpaper = category_wallpaper_list[randint(0, len(category_wallpaper_list)-1)]
+        return random_wallpaper["img_1600_900"]
+    except Exception as e:
+        print("Error: ", e)
+        return None
 
 # 根据输入的URL，通过requests库下载URL对应的壁纸到/temp目录，并返回该壁纸的路径
 def download_wallpaper(url):
@@ -28,7 +48,7 @@ def set_wallpaper(path):
         print("Error: ", e)
 
 if __name__ == '__main__':
-    wallpaper = get_random_wallpaper()
+    wallpaper = get_random_wallpaper_360()
     print("ramdom wallpaper: ", wallpaper)
     if wallpaper:
         uri = download_wallpaper(wallpaper)
